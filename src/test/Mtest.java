18c94aebe25fab719dec15c697268fed1c8d38ad
unknown <M1023849@A2ML11091.mindtree.com>
2014-09-28 10:06:39 +0530

    My first commit

---
+++ src/test/Mtest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Mtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
